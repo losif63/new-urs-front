196d279aa2b46de8d166a017e03e0b704035a74c
antaechan <0627antaechan@gmail.com>
2022-07-22 22:39:46 +0900

    Implement Login Page Layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Login from "./pages/Login";
-import MyReservation from "./pages/MyReservation";
-import Reservation from "./pages/Reservation";
+import SignUp from "./pages/SignUp";
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="navBar">
-        <li className="navBarMenu">
-          <Link to="myReservation">
-            <p className="navTitle">나의 예약</p>
-          </Link>
-        </li>
-        <li className="navBarMenu">
-          <Link to="reservation">
-            <p className="navTitle">예약</p>
-          </Link>
-        </li>
-      </nav>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="myReservation" element={<MyReservation />} />
-        <Route path="reservation" element={<Reservation />} />
+        <Route path="myReservation" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
-  return <p>Login</p>;
+  return (
+    <div className="loginPage">
+      <div className="ursTitle">KAIST URS</div>
+      <input
+        className="input"
+        type="text"
+        placeholder="아이디를 입력해주세요"
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="비밀번호를 입력해주세요"
+      />
+      <div className="button">
+        <Link to="signup" style={{ textDecoration: "none" }}>
+          회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
