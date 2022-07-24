1e8bce0bb4a6a9eb131530cfe1bcb22d2019cdb0
jaduksuh <jaduksuh@naver.com>
2022-07-24 14:05:34 +0000

    Added svg files

--- src/pages/login-succ/MyReservation.jsx
+++ src/pages/login-succ/MyReservation.jsx
@@ -2,5 +2,24 @@ import React from "react";
 import "./MyReservation.css";
 
 export default function MyReservation() {
-  return <div className="container">myResevation</div>;
+  return <div className="container">
+    <div className="content">
+      <div className="currentReservations">
+        <text className="status">현재 진행 중</text>
+        <div className="reservationList">
+          <div className="reservationInstance">
+
+          </div>
+        </div>
+      </div>
+      <div className="futureReservations">
+        <text className="status">예정</text>
+        <div className="reservationList">
+          <div className="reservationInstance">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 }
