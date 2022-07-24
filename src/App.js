561f30c9d583e5525a80cbcff3b5917ebbe06ff6
antaechan <0627antaechan@gmail.com>
2022-07-24 19:24:51 +0900

    Implement Reservation page layout with figma figure

--- src/App.js
+++ src/App.js
@@ -4,36 +4,37 @@ import SignUp from "./pages/SignUp";
 import MyReservation from "./pages/login-succ/MyReservation";
 import Reservation from "./pages/login-succ/Reservation";
 
-import { useSelector } from "react-redux";
-
 import "./App.css";
 import { useState, useEffect } from "react";
 
 export default function App() {
-  const [loginSucc, setLoginSucc] = useState(false);
+  const [loginSucc, setLoginSucc] = useState(true);
 
-  useState(()=>{
+  useEffect(() => {
     return;
   }, [loginSucc]);
-  
+
   return (
     <BrowserRouter>
       {loginSucc ? (
-        <nav className="navBar">
-          <li className="navBarMenu">
-            <Link to="myReservation" style={{ textDecoration: "none" }}>
-              <p className="navTitle">나의 예약</p>
-            </Link>
-          </li>
-          <li className="navBarMenu">
-            <Link to="reservation" style={{ textDecoration: "none" }}>
-              <p className="navTitle">예약</p>
-            </Link>
-          </li>
-          <li className="navBarMenu">
-            <p className="navTitle">QR 예약</p>
-          </li>
-        </nav>
+        <div className="header">
+          <div className="curTitle">나의 예약</div>
+          <nav className="navBar">
+            <li className="navBarMenu">
+              <Link to="myReservation" style={{ textDecoration: "none" }}>
+                <p className="navTitle">나의 예약</p>
+              </Link>
+            </li>
+            <li className="navBarMenu">
+              <Link to="reservation" style={{ textDecoration: "none" }}>
+                <p className="navTitle">예약</p>
+              </Link>
+            </li>
+            <li className="navBarMenu">
+              <p className="navTitle">QR 예약</p>
+            </li>
+          </nav>
+        </div>
       ) : (
         <></>
       )}
@@ -45,7 +46,7 @@ export default function App() {
           </>
         ) : (
           <>
-            <Route path="/" element={<Login setLoginSucc={setLoginSucc}/>} />
+            <Route path="/" element={<Login setLoginSucc={setLoginSucc} />} />
             <Route path="signup" element={<SignUp />} />
           </>
         )}
--- src/pages/login-succ/Reservation.jsx
+++ src/pages/login-succ/Reservation.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./Reservation.css";
 
 export default function Reservation() {
-  return <div className="container">Resevation</div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="textMessage">예약하실 장소를 선택해주세요</div>
+        <input
+          className="search"
+          type="text"
+          placeholder="🔍 용도, 건물 혹은 장소 검색"
+        />
+        <div className="locationHolder">
+          <div className="switch">
+            <div
+              className="forUse"
+              onClick={() => {
+                alert("용도별");
+              }}
+            >
+              용도별
+            </div>
+            <div
+              className="forLocation"
+              onClick={() => {
+                alert("건물별");
+              }}
+            >
+              건물별
+            </div>
+          </div>
+          <div className="location">
+            <img className="학술문화관" src="학술문화관.png" />
+            <img className="스포츠컴플렉스" src="스포츠컴플렉스.png" />
+            <img className="북측운동장" src="북측운동장.png" />
+            <img className="교양분관" src="교양분관.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
