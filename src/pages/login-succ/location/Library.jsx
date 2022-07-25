4fd8ee159faf787f27881ce1d06c3ead68104def
antaechan <0627antaechan@gmail.com>
2022-07-25 21:46:02 +0900

    Refactor svg file into component

--- src/pages/login-succ/location/Library.jsx
+++ src/pages/login-succ/location/Library.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Library() {
   return (
     <svg
-      width="200"
-      height="160"
+      width="150"
+      height="120"
       viewBox="0 0 100 80"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
