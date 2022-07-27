5271311247d58b7e8b743381bd3963cb0fda77e9
jaduksuh <jaduksuh@naver.com>
2022-07-27 13:56:55 +0000

    Added cancel reservation

--- src/pages/login-succ/MyReservation.jsx
+++ src/pages/login-succ/MyReservation.jsx
@@ -1,17 +1,75 @@
 import React, { useState,useEffect } from "react";
 import { useSelector } from "react-redux";
+import ReactModal from "react-modal";
 import axios from "axios";
 import "./MyReservation.css";
 
 function ReservationInstance(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  ReactModal.setAppElement('#root');
+  const deleteReservation = async () => {
+    await axios.patch(`${process.env.REACT_APP_SERVER_URL}/reservation/cancelreservation`, {
+      u_id: props.u_id,
+      r_id: props.r_id
+    });
+    props.setReservations(props.reservations.filter((value, index, arr) => {
+      return value.r_id != props.r_id;
+    }));
+  }
   return <div className="reservationInstance">
     <div className="textArea">
       <text className="room_name">{props.room_name}</text> <br></br>
       <text className="building_name">{props.building_name}</text> <br></br>
+      <div style={{
+        width: "100%",
+        textAlign: "center",
+        borderBottom: "1px solid #aaa",
+        lineHeight: "0.1em",
+        margin: "30px 0 10px"
+      }}>
+      </div>
       <div className="dateTime">
         <text className="dateInfo">{props.dateInfo}</text> <br></br>
         <text className="timeInfo">{props.timeInfo}</text> <br></br>
       </div>
+      <button onClick={() => setModalIsOpen(true)}>Open</button>
+      <ReactModal isOpen={modalIsOpen} style={{
+        overlay: {
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(50, 50, 50, 0.9)'
+        },
+        content: {
+          position: 'absolute',
+          top: '15%',
+          left: '40%',
+          right: '40%',
+          bottom: '15%',
+          border: '1px solid #ccc',
+          background: '#fff',
+          overflow: 'auto',
+          WebkitOverflowScrolling: 'touch',
+          borderRadius: '27px',
+          outline: 'none',
+          padding: '20px'
+        }
+      }}>
+        <button onClick={() => setModalIsOpen(false)}><span aria-hidden="true">&times;</span></button>
+        <div className="modalTitle">
+          <text>{props.room_name}</text><br></br>
+          <text>{props.building_name}</text><br></br>
+        </div>
+        <text>{props.room_name}</text><br></br>
+        <text>{props.building_name}</text><br></br>
+        <text>{props.dateInfo}</text><br></br>
+        <text>{props.timeInfo}</text><br></br>
+        <text>{props.username}</text><br></br>
+        <text>{props.activity_name}</text><br></br>
+        <button onClick={deleteReservation}>예약 취소</button>
+      </ReactModal>
     </div>
   </div>;
 }
@@ -68,7 +126,7 @@ function ReservationList(props) {
 
   return <div className="reservationList">
     {reservations.map(t => {
-      return <ReservationInstance room_name={t.room_name} building_name={t.building_name} dateInfo={t.start_time.substring(0, 10)} timeInfo={t.start_time.substring(11, 19) + ' ~ ' + t.finish_time.substring(11, 19)}></ReservationInstance>
+      return <ReservationInstance reservations={reservations} setReservations={setReservations} u_id={uId} r_id={t.r_id} room_name={t.room_name} building_name={t.building_name} dateInfo={t.start_time.substring(0, 10)} timeInfo={t.start_time.substring(11, 19) + ' ~ ' + t.finish_time.substring(11, 19)} username={t.username} activity_name={t.activity_name}></ReservationInstance>
     })}
   </div>;
 }
