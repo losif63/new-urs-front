a91d498ef22f42ff53c7410d216756f41d7588f0
antaechan <0627antaechan@gmail.com>
2022-07-27 23:44:08 +0900

    Add user name field at signUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,38 +4,39 @@ import { useDispatch } from "react-redux";
 import axios from "axios";
 
 import signUpSlice from "./../signUpSlice";
+import reservationSlice from "../reservationSlice";
 
 import "./SignUp.css";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
 
   const SignUpHandler = async () => {
-    console.log("email", email);
-    console.log("password", password);
-
-    if (email === "" || password === "") {
-      return window.alert("이메일과 비밀번호를 모두 입력해주세요");
+    if (name === "" || email === "" || password === "") {
+      return window.alert("이름, 이메일, 비밀번호를 모두 입력해주세요");
     }
 
     try {
       const response = await axios.post(
         `${process.env.REACT_APP_SERVER_URL}/user/createuser`,
         {
+          name: name,
           email: email,
           password: password,
         }
       );
 
       if (response.status === 201) {
+        dispatch(reservationSlice.actions.writeName(name));
         dispatch(signUpSlice.actions.writeEmail(email));
         dispatch(signUpSlice.actions.writePassword(password));
         return window.alert("회원가입이 성공적으로 완료되었습니다");
-      } 
+      }
     } catch (e) {
-      if(e.response.status === 409) {
+      if (e.response.status === 409) {
         return window.alert("이미 존재하는 이메일입니다");
       } else {
         return window.alert(`Error: ${e}`);
@@ -47,14 +48,21 @@ export default function SignUp() {
     <div className="loginPage">
       <div className="ursTitle">KAIST URS</div>
       <input
-        className="input"
+        className="signUpInput"
+        type="text"
+        placeholder="사용하실 이용자 이름을 입력해주세요"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        className="signUpInput"
         type="text"
         placeholder="사용하실 이메일을 입력해주세요"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
-        className="input"
+        className="signUpInput"
         type="text"
         placeholder="사용하실 비밀번호를 입력해주세요"
         value={password}
